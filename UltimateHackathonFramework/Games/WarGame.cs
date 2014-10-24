af155218bc5b993c8e96e51136b883a4e428a4f0
arikin <arikin6@gmail.com>
2014-10-24 23:52:58 +0200

    WarGame

---
+++ UltimateHackathonFramework/Games/WarGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateHackathonFramework.Interfaces;
+
+namespace UltimateHackathonFramework.Games
+{
+    public class WarGame : IGame
+    {
+
+        public IResult Result
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void StartAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(IList<IBot> bots)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
