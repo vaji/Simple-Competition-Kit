a4bb391deb0ed431767863320a668d533df2908b
mgulik21 <mgulik21@gmail.com>
2014-10-24 22:38:36 +0200

    Add IClientManager

---
+++ UltimateHackathonFramework/Interfaces/IClientManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateHackathonFramework.Interfaces
+{
+    interface IClientManager
+    {
+        private IList<IBot> clients;
+
+        public IList<IBot> Clients
+        {
+            get { return clients; }
+            set { clients = value; }
+        }
+        
+        public void ScanForClients();
+        
+    }
+}
