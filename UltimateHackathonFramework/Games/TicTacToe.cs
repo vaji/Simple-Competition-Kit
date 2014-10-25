34b404c2e64e0d6a46754507385cf7af3db161e2
arikin <arikin6@gmail.com>
2014-10-25 20:33:13 +0200

    TTBot2

--- UltimateHackathonFramework/Games/TicTacToe.cs
+++ UltimateHackathonFramework/Games/TicTacToe.cs
@@ -9,37 +9,21 @@ using UltimateHackathonFramework.Models;
 namespace UltimateHackathonFramework.Games
 {
 
-    
-    enum CellStateEnum {Free, X, O};
-    
-    internal static class Helpers
-    {
-        public static char ToChar(this CellStateEnum state)
-        {
-            switch(state)
-            {
-                case CellStateEnum.Free: return ' ';
-                case CellStateEnum.X: return 'X';
-                case CellStateEnum.O: return 'O';
-                default: return 'E';
-            }
-        }
-    }
+    enum CellStateEnum {clFree, clTakenX, clTakenO};
 
     class Cell
     {
-        public CellStateEnum CellState = CellStateEnum.Free;
+        public CellStateEnum CellState = CellStateEnum.clFree;
     }
 
     class TicTacToe:Game
     {
         Cell[,] Grid;
-        private readonly int CELLX = 3;
-        private readonly int CELLY = 3;
+
 
         protected override IResult DoRound(IList<IBot> bots)
         {
-            Grid = new Cell[CELLX, CELLY];
+            Grid = new Cell[3, 3];
             for (int x = 0; x < 3; x++)
             {
                 for (int y = 0; y < 3; y++)
@@ -54,14 +38,10 @@ namespace UltimateHackathonFramework.Games
             Dictionary<string, string> XOMapper = new Dictionary<string, string>();
             XOMapper.Add(bots[0].ID, "X");
             XOMapper.Add(bots[1].ID, "O");
-            Result.addFinalResult("***********************");
-            Result.addFinalResult(bots[0].ID + " - X");
-            Result.addFinalResult(bots[1].ID + " - O");
+
             for (int iterator = 0; iterator < 9; iterator++)
             {
-                int currentPlayer = iterator % 2;
-                Dictionary<string, string> response = bots[currentPlayer].Communicate(new Dictionary<string, string>() { { "move", "" } });
-                _result.addToLog(bots[currentPlayer].ID, response);
+                Dictionary<string, string> response = bots[iterator % 2].Communicate(new Dictionary<string, string>() { { "move", "" } });
                 if(response.ContainsKey("move") && response["move"]!="")
                 {
                     int targetCell = -1;
@@ -71,30 +51,30 @@ namespace UltimateHackathonFramework.Games
                     }
                     catch (Exception)
                     {
-                        bots[currentPlayer].CurrentState = Enums.State.Failed;
-                        bots[currentPlayer].Communicate(new Dictionary<string, string>() { { "error", "TargetCellInfoCorrupted" } });
-                        bots[(iterator + 1) % 2].AddPoints(2);
+                        bots[iterator % 2].CurrentState = Enums.State.Failed;
+                        bots[iterator % 2].Communicate(new Dictionary<string, string>() { { "error", "TargetCellInfoCorrupted" } });
+                        bots[(iterator + 1) % 2].addPoints(2);
                         break;
                     }
                     if(targetCell>8)
                     {
-                        bots[currentPlayer].CurrentState = Enums.State.Failed;
-                        bots[currentPlayer].Communicate(new Dictionary<string, string>() { { "error", "TargetCellOutOfRange" } });
-                        bots[(iterator + 1) % 2].AddPoints(2);
+                        bots[iterator % 2].CurrentState = Enums.State.Failed;
+                        bots[iterator % 2].Communicate(new Dictionary<string, string>() { { "error", "TargetCellOutOfRange" } });
+                        bots[(iterator + 1) % 2].addPoints(2);
                         break;
 
                     }
                     int x = targetCell % 3;
                     int y = targetCell / 3;
-                    if(Grid[x, y].CellState==CellStateEnum.Free)
+                    if(Grid[x, y].CellState==CellStateEnum.clFree)
                     {
                         if (XOMapper[bots[iterator % 2].ID] == "X")
                         {
-                            Grid[x, y].CellState = CellStateEnum.X;
+                            Grid[x, y].CellState = CellStateEnum.clTakenX;
                         }
                         if (XOMapper[bots[iterator % 2].ID] == "O")
                         {
-                            Grid[x, y].CellState = CellStateEnum.O;
+                            Grid[x, y].CellState = CellStateEnum.clTakenO;
                         }
 
                         foreach (Bot bot in bots)
@@ -104,37 +84,30 @@ namespace UltimateHackathonFramework.Games
                     }
                     else
                     {
-                        bots[currentPlayer].CurrentState = Enums.State.Failed;
-                        bots[currentPlayer].Communicate(new Dictionary<string, string>() { { "error", "TargetedCell is already taken!" } });
-                        bots[(iterator + 1) % 2].AddPoints(2);
+                        bots[iterator % 2].CurrentState = Enums.State.Failed;
+                        bots[iterator % 2].Communicate(new Dictionary<string, string>() { { "error", "TargetedCell is already taken!" } });
+                        bots[(iterator + 1) % 2].addPoints(2);
                         break;
                     }
-                    CellStateEnum status = verifyVictory(Grid);
-                    if(status!=CellStateEnum.Free)
+                    CellStateEnum result = verifyVictory(Grid);
+                    if(result!=CellStateEnum.clFree)
                     {
-                        Result.addFinalResult(PrintGrid(Grid));
-                        if(status==CellStateEnum.O)
+                        if(result==CellStateEnum.clTakenO)
                         {
-                            foreach (Bot bot in bots)
+       foreach (Bot bot in bots)
                             {
                                 bot.Communicate(new Dictionary<string,string>(){{"win", "O"}});
                                 if (XOMapper[bot.ID] == "O")
-                                {
-                                    Result.addFinalResult(bot.ID + " won as 0");
-                                    bot.AddPoints(3);
-                                }
+                                    bot.addPoints(3);
                             }
                         }
-                        if (status == CellStateEnum.X)
+                        if (result == CellStateEnum.clTakenX)
                         {
                             foreach (Bot bot in bots)
                             {
                                 bot.Communicate(new Dictionary<string, string>() { { "win", "X" } });
                                 if (XOMapper[bot.ID] == "X")
-                                {
-                                    Result.addFinalResult(bot.ID + " won as X");
-                                    bot.AddPoints(3);
-                                }
+                                    bot.addPoints(3);
 
                             }
                         }
@@ -147,7 +120,7 @@ namespace UltimateHackathonFramework.Games
                         {
                             for (int iy = 0; iy < 3; iy++)
                             {
-                                if (Grid[ix, iy].CellState == CellStateEnum.Free)
+                                if (Grid[ix, iy].CellState == CellStateEnum.clFree)
                                     count++;
                             }
                         }
@@ -156,11 +129,9 @@ namespace UltimateHackathonFramework.Games
                             foreach (Bot bot in bots)
                             {
                                 bot.Communicate(new Dictionary<string, string>() { { "tie", "" } });
-                                bot.AddPoints(1);
-                                
+                                bot.addPoints(1);
+
                             }
-                            Result.addFinalResult(PrintGrid(Grid));
-                            Result.addFinalResult("Draw.");
                             break;
                         }
                     }
@@ -169,43 +140,24 @@ namespace UltimateHackathonFramework.Games
                 {
                     bots[iterator % 2].CurrentState = Enums.State.Failed;
                     bots[iterator % 2].Communicate(new Dictionary<string, string>() { { "error", "CorruptedMoveResponseException" } });
-                    bots[(iterator + 1) % 2].AddPoints(2);
+                    bots[(iterator + 1) % 2].addPoints(2);
                     break;
                 }
             }
             return Result;
         }
 
-        private string PrintGrid(Cell[,] Grid)
-        {
-            var sb = new StringBuilder();
-            for(int i = 0; i < CELLY; i++)
-            {
-                sb.AppendLine();
-                sb.Append("|");
-                for (int j = 0; j < CELLX; j++)
-                {
-                    sb.Append(Grid[j, i].CellState.ToChar());
-                    sb.Append("|");
-                }
-
-
-            }
-            sb.AppendLine();
-            return sb.ToString();
-        }
-
         private CellStateEnum verifyVictory(Cell[,] Grid)
         {
             for (int i = 0; i < 3; i++)
                 {
-                if (Grid[0, i].CellState == Grid[1, i].CellState && Grid[0, i].CellState == Grid[2, i].CellState && Grid[0, i].CellState != CellStateEnum.Free) return Grid[0, i].CellState;
-                if (Grid[i, 0].CellState == Grid[i, 1].CellState && Grid[i, 0].CellState == Grid[i, 2].CellState && Grid[i, 0].CellState != CellStateEnum.Free) return Grid[i, 0].CellState; 
+                if (Grid[0, i].CellState == Grid[1, i].CellState && Grid[0, i].CellState == Grid[2, i].CellState && Grid[0, i].CellState != CellStateEnum.clFree) return Grid[0, i].CellState;
+                if (Grid[i, 0].CellState == Grid[i, 1].CellState && Grid[i, 0].CellState == Grid[i, 2].CellState && Grid[i, 0].CellState != CellStateEnum.clFree) return Grid[i, 0].CellState; 
                 }
-            if (Grid[0, 0].CellState == Grid[1, 1].CellState && Grid[0, 0].CellState == Grid[2, 2].CellState && Grid[0, 0].CellState != CellStateEnum.Free) return Grid[0, 0].CellState;
-            if (Grid[0, 2].CellState == Grid[1, 1].CellState && Grid[0, 2].CellState == Grid[2, 0].CellState && Grid[0, 2].CellState != CellStateEnum.Free) return Grid[0, 2].CellState;
+            if (Grid[0, 0].CellState == Grid[1, 1].CellState && Grid[0, 0].CellState == Grid[2, 2].CellState && Grid[0, 0].CellState != CellStateEnum.clFree) return Grid[0, 0].CellState;
+            if (Grid[0, 2].CellState == Grid[1, 1].CellState && Grid[0, 2].CellState == Grid[2, 0].CellState && Grid[0, 2].CellState != CellStateEnum.clFree) return Grid[0, 2].CellState;
 
-            return CellStateEnum.Free;
+            return CellStateEnum.clFree;
             }
         }
 
