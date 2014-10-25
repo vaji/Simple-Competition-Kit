8df2f5de346015b40fbcf3ee30049ef150603f18
Awesomovski <d.materowski@gmail.com>
2014-10-25 03:40:52 +0200

    UI

--- UltimateHackathonFramework/ViewModels/ClientsViewModel.cs
+++ UltimateHackathonFramework/ViewModels/ClientsViewModel.cs
@@ -1,6 +1,8 @@
 
+using Caliburn.Micro;
 using System.Collections.Generic;
 using UltimateHackathonFramework.Interfaces;
+using UltimateHackathonFramework.Models;
 namespace UltimateHackathonFramework
 {
     public class ClientsViewModel : Caliburn.Micro.PropertyChangedBase
@@ -9,14 +11,54 @@ namespace UltimateHackathonFramework
         public ClientsViewModel(IClientManager clientManager)
         {
             _clientManager = clientManager;
+            Bots = new BindableCollection<IBot>();
+            Bots.AddRange(clientManager.Clients);
+            SelectedBots = new BindableCollection<IBot>();
         }
 
-        public IList<IBot> Bots;
-        public IList<IBot> SelectedBots;
+        public IObservableCollection<IBot> Bots { get; private set; }
+        public IObservableCollection<IBot> SelectedBots { get; private set; }
 
 
 
+        private IBot _selectedBotFromListOfAll;
+
+        public IBot SelectedBotFromListOfAll
+        {
+            get { return _selectedBotFromListOfAll; }
+            set { _selectedBotFromListOfAll = value; 
+                NotifyOfPropertyChange(() => SelectedBotFromListOfAll);
+                NotifyOfPropertyChange(() => CanAddBot);
+            }
+        }
+
+        private IBot _selectedBotFromSelectedList;
+
+        public IBot SelectedBotFromSelectedList
+        {
+            get { return _selectedBotFromSelectedList; }
+            set { _selectedBotFromSelectedList = value;
+                NotifyOfPropertyChange(() => SelectedBotFromSelectedList);
+                NotifyOfPropertyChange(() => CanRemoveBot);
+            }
+        }
 
 
+        public void AddBot() { SelectedBots.Add(SelectedBotFromListOfAll); }
+        public bool CanAddBot { get { return SelectedBotFromListOfAll != null; } }
+
+
+        public void RemoveBot() { SelectedBots.Remove(SelectedBotFromSelectedList); }
+        public bool CanRemoveBot { get { return SelectedBotFromSelectedList != null; } }
+
+
+        public void ScanForClients()
+        {
+            _clientManager.ScanForClients();
+            Bots.Clear();
+            Bots.AddRange(_clientManager.Clients);
+            SelectedBots.Clear();
+
+        }
     }
 }
