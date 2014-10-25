9f6755abcad5a777b0718542413095b6718ff3a3
arikin <arikin6@gmail.com>
2014-10-25 07:31:44 +0200

    FakeBot

--- FakeBot/Program.cs
+++ FakeBot/Program.cs
@@ -1,44 +1,79 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Diagnostics;
 
 namespace FakeBot
 {
-    class Program
+   
+   public  class Program
     {
         static Communication communication = null;
+        static TTTBot bot;
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]+"  "+args[1]);
+        
+            Console.WriteLine(args[0] + "  " + args[1]);
             communication = new Communication();
             communication.Connect(args[0], Convert.ToInt32(args[1]));
             if(communication.connected)
             {
                 Console.WriteLine("connected");
+               bot = new TTTBot();
+
+                bot.Init();
+                while (!bot.game_finished)
+                {
+
+                   
+                    
+                }
+               
+
+
             }
             else
             {
                 Console.WriteLine("Not connected");
             }
-            communication.ServerWriteEvent += communication_ServerWriteEvent;
-            communication.ServerExceptionEvent += communication_ServerExceptionEvent;
-            Console.ReadLine();
-            communication.disconnect();
-
+            
         }
 
-        static void communication_ServerExceptionEvent(Exception exception)
+         static void communication_ServerExceptionEvent(Exception exception)
         {
 
         }
 
-        static void communication_ServerWriteEvent(string message)
+
+        public static Dictionary<string, string> Communicate(string data)
+        {
+           // 
+            string json = data;
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            }
+            catch (Exception)
+            {
+
+
+            }
+            return null;
+        }
+       public static void communication_ServerWriteEvent(string message)
         {
             Console.WriteLine("Server said: " + message);
-            communication.SendString(message);
+            Dictionary<string,string> BotRespond =  bot.Play(Communicate(message));
+
+            string json = new JavaScriptSerializer().Serialize(BotRespond.ToDictionary(item => item.Key.ToString(), item => item.Value.ToString()));
+
+            communication.SendString(json);
             
         }
     }
